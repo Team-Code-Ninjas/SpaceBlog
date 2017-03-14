28b4b57a5c9136688f38e3210b398f8e179ede31
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-03-14 11:09:22 +0200

    Create Space Blog Create Header and add home background image

---
+++ SpaceBlog/SpaceBlog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpaceBlog.MvcApplication" Language="C#" %>
---
+++ SpaceBlog/SpaceBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpaceBlog.Startup))]
+namespace SpaceBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
