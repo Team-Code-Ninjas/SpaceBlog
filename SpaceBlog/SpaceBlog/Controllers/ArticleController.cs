113956cfb48b9d14afa0c393275069de808e0347
rightdirection1 <lyubima.takova@gmail.com>
2017-03-17 00:10:41 +0200

    Create page of Article

---
+++ SpaceBlog/SpaceBlog/Controllers/ArticleController.cs
@@ -0,0 +1,22 @@
+﻿using SpaceBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpaceBlog.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            Article[] articles = new Article[]
+            {
+                new Article {title = "exampleTitle", Author = "authorName"}
+            };
+            return View(articles);
+        }
+    }
+}
---
+++ SpaceBlog/SpaceBlog/Models/Article.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpaceBlog.Models
+{
+    public class Article
+    {
+        public string title { get; set; }
+        public string Author { get; set; }
+    }
+}
