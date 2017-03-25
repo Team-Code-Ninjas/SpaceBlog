d8a67d6eeb58f8a5bec4b06271acd9a4804866a6
Alexcho0o0 <alex3452>
2017-03-25 19:11:27 +0200

    Created article functionality. Features like Create, Edit, Delete and View were added. Articles are stored in the database.

--- SpaceBlog/SpaceBlog/Models/Article.cs
+++ SpaceBlog/SpaceBlog/Models/Article.cs
@@ -4,6 +4,11 @@ namespace SpaceBlog.Models
 {
     public class Article
     {
+        public Article()
+        {
+            Date = DateTime.Now;
+        }
+
         public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
