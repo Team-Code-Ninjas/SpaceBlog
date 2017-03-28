02029a3d0b70e650ce992a2813e535c9f38d3363
Alexcho0o0 <alex3452>
2017-03-28 19:27:28 +0300

    Articles with no content and/or title can no longer be created

--- SpaceBlog/SpaceBlog/Models/Article.cs
+++ SpaceBlog/SpaceBlog/Models/Article.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SpaceBlog.Models
 {
@@ -9,13 +10,22 @@ namespace SpaceBlog.Models
             Date = DateTime.Now;
         }
 
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Title { get; set; }
+
+        [Required]
         public string Content { get; set; }
+
         public DateTime Date { get; set; }
+
         //public virtual IEnumerable<Comment> Comments { get; set; }
         //public Category Category { get; set; }
         //public virtual IEnumerable<Tag> Tags  { get; set; }
+
         public ApplicationUser Author { get; set; }
     }
 }
