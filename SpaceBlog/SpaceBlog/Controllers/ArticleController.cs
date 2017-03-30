5886f43e55a196ab9f97b597d55176ea76d88a0e
Alexcho0o0 <XelaxStanev@gmailc.com>
2017-03-31 02:36:28 +0300

    Users can now create articles with html tags in them. For example <image> or <b> <h1>
    NOTE: Scripts can also be used! That in a normal blog will never be acceptable because it can be dangerous.

--- SpaceBlog/SpaceBlog/Controllers/ArticleController.cs
+++ SpaceBlog/SpaceBlog/Controllers/ArticleController.cs
@@ -43,6 +43,7 @@ namespace SpaceBlog.Models
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [ValidateInput(false)]
         [ValidateAntiForgeryToken]
         [Authorize]
         public ActionResult Create([Bind(Include = "Id,Title,Content")] Article article)
