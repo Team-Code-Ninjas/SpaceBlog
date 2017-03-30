0e93af98f2d5f82cf599908ff407d812fdbb548b
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-03-30 12:09:34 +0300

    Create class Utilities and method CutText

---
+++ SpaceBlog/SpaceBlog/Utilities/Utils.cs
@@ -0,0 +1,25 @@
+﻿namespace SpaceBlog.Utilities
+{
+    public class Utils
+    {
+        /// <summary>
+        /// This method will be used to display only the first 
+        /// 100 characters of each content in article
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="maxLength"></param>
+        /// <returns>text</returns>
+        /// <returns>shortText</returns>
+        public static string CutText(string text, int maxLength = 100)
+        {
+            if (text == null || text.Length <= maxLength)
+            {
+                return text;
+            }
+
+            var shortText = text.Substring(0, maxLength) + "...";
+
+            return shortText;
+        }
+    }
+}
