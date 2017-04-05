6515504c3411c0194ad339f954db46d0b352aa55
CCoDe4 <gory_stefanov@mail.bg>
2017-04-05 17:53:18 +0300

    add back contact and comment models

---
+++ SpaceBlog/SpaceBlog/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpaceBlog.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public ApplicationUser Author { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public Article Article { get; set; }
+    }
+}
