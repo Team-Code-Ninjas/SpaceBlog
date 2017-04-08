13ef7fcda2416392ebc40f4841f0650dd5196a68
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-04-08 12:18:31 +0300

    Add DataTables

--- SpaceBlog/SpaceBlog/App_Start/BundleConfig.cs
+++ SpaceBlog/SpaceBlog/App_Start/BundleConfig.cs
@@ -4,7 +4,6 @@
 
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -13,8 +12,6 @@
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -29,6 +26,10 @@
             bundles.Add(new ScriptBundle("~/bundles/tinymce").Include(
                       "~/Scripts/Tinymce/tinymce.min.js",
                       "~/Scripts/PageScripts/article-editor.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                      "~/Scripts/DataTables/jquery.dataTables.min.js",
+                      "~/Scripts/DataTables/dataTables.bootstrap.min.js"));
         }
     }
 }
