637f9b8b66ea80295e0005f46cf4cfc523824caa
CCoDe4 <gory_stefanov@mail.bg>
2017-04-12 18:17:03 +0300

    Added Rating model.

---
+++ SpaceBlog/SpaceBlog/Models/Rating.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SpaceBlog.Models
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+
+        [Range(1, 5)]
+        public decimal Value { get; set; }
+
+        public string AuthorId { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public virtual ApplicationUser Author { get; set; }
+
+        public int ArticleId { get; set; }
+
+        [ForeignKey("ArticleId")]
+        public virtual Article Article { get; set; }
+    }
+}
