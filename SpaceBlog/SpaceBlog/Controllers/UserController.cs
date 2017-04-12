6205f09cc2ab2d275857d3dfd5fe61ca6e3996ce
rightdirection1 <lyubima.takova@gmail.com>
2017-04-12 14:38:34 +0300

    Created Controlller,Fist View and Use Model ApplicationUser

---
+++ SpaceBlog/SpaceBlog/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using SpaceBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpaceBlog.Controllers
+{
+    public class UserController : Controller
+    {
+        private BlogDBContext db = new BlogDBContext();
+        // GET: User
+        [ActionName("Profile")]
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
