fedbfd6a1ca0b48fde7c3dcbbab1347a07c01496
CCoDe4 <gory_stefanov@mail.bg>
2017-04-12 18:15:38 +0300

    Added Rating Controller.

---
+++ SpaceBlog/SpaceBlog/Controllers/RatingController.cs
@@ -0,0 +1,18 @@
+﻿using SpaceBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpaceBlog.Controllers
+{
+    public class RatingController : Controller
+    {
+        private readonly BlogDBContext db = new BlogDBContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
